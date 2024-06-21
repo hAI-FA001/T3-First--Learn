f4d132aa803af32725aec8022b84e1cdcbb6ab9f
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-06-21 13:29:14 +0500

    linking DB w/ UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,31 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/1233f194-b8fb-4fae-a601-f011ab3e9b11-1sj3pb.png",
-  "https://utfs.io/f/ac72614b-dbb0-4843-819c-71138dd8cd4c-1lv0vz.png",
-  "https://utfs.io/f/368abcb0-26f4-4165-bf5d-02823fff88b6-hcfblw.png",
-  "https://utfs.io/f/db313d57-4e40-48cd-acfa-35d99b4c3204-ckolpg.png"
-]
+// not using these now, cuz added to DB
+// const mockUrls = [
+//   "https://utfs.io/f/1233f194-b8fb-4fae-a601-f011ab3e9b11-1sj3pb.png",
+//   "https://utfs.io/f/ac72614b-dbb0-4843-819c-71138dd8cd4c-1lv0vz.png",
+//   "https://utfs.io/f/368abcb0-26f4-4165-bf5d-02823fff88b6-hcfblw.png",
+//   "https://utfs.io/f/db313d57-4e40-48cd-acfa-35d99b4c3204-ckolpg.png"
+// ]
 
-const mockImages = mockUrls.map((url, idx) => ({  // need to put {...} in ()
-  id: idx + 1,
-  url
-}));
+// const mockImages = mockUrls.map((url, idx) => ({  // need to put {...} in ()
+//   id: idx + 1,
+//   url
+// }));
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, {desc}) => desc(model.id)
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">  {/* gap-4 to put space b/w images */}
-      {posts.map((post) => (<div key={post.id}>{post.name}</div>))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
+      {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 flex flex-col">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
     </div>
