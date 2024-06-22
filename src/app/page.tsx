0d5c6a6d480176093274d57cdc7db3ad8edcbe6e
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-06-22 06:23:56 +0500

    grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,8 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {images.map((image) => (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {[...images, ...images, ...images, ...images].map((image) => (
         <div key={image.id} className="h-48 w-48 flex flex-col">
           <Link href={`/img/${image.id}`}>
             <Image src={image.url} style={{ objectFit: "contain" }} width={192} height={192} alt={image.name} />
