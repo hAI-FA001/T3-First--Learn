f384b2f716652b92b483e02b5c87b6e1053cef48
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-07-04 14:16:06 +0500

    some fixes/additions (view album button, image modal in view album)

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,17 +1,17 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-import { SimpleUploadButton } from './simple-upload-button';
-import Link from 'next/link';
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SimpleUploadButton } from "./simple-upload-button";
+import ViewAlbumButton from "./view-album-button";
 
 export function TopNav() {
   return (
-    <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b">
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div className='flex flex-row items-center gap-4'>
-        <Link href="/album/view" className="bg-blue-950 text-white hover:bg-blue-800 rounded-md p-3 me-5">View Albums</Link>
+      <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <ViewAlbumButton />
           {/* will get type error if it's incorrect, it's taking this from uploadthing core.ts */}
           {/* <UploadButton endpoint="imageUploader" onClientUploadComplete={() => {
                     router.refresh();
@@ -21,5 +21,5 @@ export function TopNav() {
         </SignedIn>
       </div>
     </nav>
-  )
+  );
 }
